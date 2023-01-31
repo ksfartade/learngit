e84c75974b5701b4a0c1ccaa2774851b2562e4e5
Kiran Fartade <kiran.fartade@mindstix.com>
2023-01-31 13:53:24 +0530

    updating newly changed file of multiplication

---
+++ test2.java
@@ -0,0 +1,7 @@
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("multiplying class");
+        int a = 5, b = 4;
+        System.out.println("multiplication is: " + a*b);
+    }
+}
